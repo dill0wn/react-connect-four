6491498172b3400ae3b8b943f8bc961df70e34a1
dill0wn <dillon@yeticgi.com>
2020-01-13 19:07:46 -0500

    Updates

---
+++ precache-manifest.430a2d0b739925c41c1b929e9e21a5b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff97dba0f00d5f244ed130401afdbe6",
+    "url": "/react-connect-four/index.html"
+  },
+  {
+    "revision": "9935278e1d318bbc7a66",
+    "url": "/react-connect-four/static/css/main.b6991318.chunk.css"
+  },
+  {
+    "revision": "44412a3cd836f5d239fc",
+    "url": "/react-connect-four/static/js/2.74caac74.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-connect-four/static/js/2.74caac74.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9935278e1d318bbc7a66",
+    "url": "/react-connect-four/static/js/main.0bee4b70.chunk.js"
+  },
+  {
+    "revision": "2bda65e5e0f14e25f137",
+    "url": "/react-connect-four/static/js/runtime-main.5bb70f13.js"
+  }
+]);
